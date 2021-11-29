7eaf42fca8a5ccf9bb8214d0473f142800d42eaa
Geoff-Walker <geoffwalker1980@gmail.com>
2021-11-29 14:09:22 +0000

    started to look at data extraction

---
+++ script.js
@@ -0,0 +1,16 @@
+// fetch('./data.json')
+// 	.then((response) => {
+// 		return response.json();
+// 	})
+// 	.then((data) => console.log(data));
+
+// const data = fetch('./data.json')
+// 	.then((response) => {
+// 		return response.json().stringify();
+// 	})
+// 	.then((data) => console.log(data));
+
+// console.log(data);
+
+// var data = JSON.parse(ajaxFunction('/data.json'));
+// console.log(data);
